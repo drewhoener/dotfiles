6fa281a96453b71d6190b0ac9b70bdcef884192a
Drew Hoener <drewhoener@gmail.com>
2019-01-17 23:32:28 -0500

    Added config files

---
+++ .config/bspwm/rules.sh
@@ -0,0 +1,11 @@
+bspc rule -a URxvt:urxvt -o node=@^1:^1:/1 follow=off
+bspc rule -a URxvt:urxvt -o node=@^1:^1:/2 follow=off
+bspc rule -a Chromium:chromium -o node=@^1:^2:/ follow=off
+bspc rule -a discord:discord -o node=@^1:^3:/1 follow=off
+bspc rule -a Messenger for Desktop:messenger for desktop -o node=@^1:^3:/2 follow=off
+bspc rule -a jetbrains-idea:sun-awt-X11-XFramePeer -o node=@^1:^4:/ follow=off
+bspc rule -a Steam:Steam -o node=@^1:^5:/ follow=off
+bspc rule -a URxvt:urxvt -o node=@^1:^9:/1 follow=off
+bspc rule -a URxvt:urxvt -o node=@^1:^9:/2 follow=off
+bspc rule -a URxvt:urxvt -o node=@^1:^10:/1 follow=off
+bspc rule -a Pavucontrol:pavucontrol -o node=@^1:^10:/2 follow=off
